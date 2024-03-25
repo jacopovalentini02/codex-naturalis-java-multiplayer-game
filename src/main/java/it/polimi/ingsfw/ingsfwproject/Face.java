fc4bf95bf8786b8b64d1d7f9ac504ba90ac43bd0
miche <michelangelo.spandri1@mail.polimi.it>
2024-03-25 16:05:13 +0100

    getters dei singoli angoli di Face e setters per segnarli come coperti/scoperti.
    prima implementazione di playCard in PlayerGround.java

--- src/main/java/it/polimi/ingsfw/ingsfwproject/Face.java
+++ src/main/java/it/polimi/ingsfw/ingsfwproject/Face.java
@@ -3,8 +3,19 @@ package it.polimi.ingsfw.ingsfwproject;
 import java.util.*;
 
 abstract public class Face {
-    private Content[] cornerList=new Content[4];
+    /*cornerList[0] = top left
+    cornerList[1] = top right
+    cornerList[2] = bottom left
+    cornerList[3] = bottom right
+     */
+
+    private Content[] cornerList;
     private boolean[] coveredCorner;
+
+    public Face(){
+        cornerList = new Content[4];
+        coveredCorner = new boolean[4];
+    }
 
     public Content[] getCornerList() {
         return cornerList;
@@ -20,5 +31,31 @@ abstract public class Face {
 
     public void setCoveredCorner(boolean[] coveredCorner) {
         this.coveredCorner = coveredCorner;
+    }
+
+    public Content getTL(){
+        return this.cornerList[0];
+    }
+    public Content getTR(){
+        return this.cornerList[1];
+    }
+    public Content getBL(){
+        return this.cornerList[2];
+    }
+    public Content getBR(){
+        return this.cornerList[3];
+    }
+
+    public void setCoveredTL(boolean value){
+        this.coveredCorner[0] = value;
+    }
+    public void setCoveredTR(boolean value){
+        this.coveredCorner[1] = value;
+    }
+    public void setCoveredBL(boolean value){
+        this.coveredCorner[2] = value;
+    }
+    public void setCoveredBR(boolean value){
+        this.coveredCorner[3] = value;
     }
 }
--- src/main/java/it/polimi/ingsfw/ingsfwproject/PlayerGround.java
+++ src/main/java/it/polimi/ingsfw/ingsfwproject/PlayerGround.java
@@ -3,13 +3,117 @@ package it.polimi.ingsfw.ingsfwproject;
 import java.util.*;
 
 public class PlayerGround {
+    /*resourceCount[0] = PLANT_KINGDOM
+    resourceCount[1] = ANIMAL_KINGDOM
+    resourceCount[2] = FUNGI_KINGDOM
+    resourceCount[3] = INSECT_KINGDOM
+
+    objectCount[0] =  QUILL
+    objectCount[1] =  INKWELL
+    objectCount[2] =  MANUSCRIPT
+
+    * */
     private int[] resourceCount;
     private int[] objectCount;
     private Map<Coordinate, Face> grid;
+    private ArrayList<Coordinate> availablePositions;
+
+    public PlayerGround(){
+        resourceCount = new int[4];
+        objectCount = new int[3];
+        grid = new HashMap<>();
+        availablePositions = new ArrayList<>();
+        availablePositions.add(new Coordinate(0,0));
+    }
 
     public void playCard(Face face, Coordinate coord){
-
+        grid.put(coord, face);
+        updateAvailablePositions(coord);
     }
+
+    private void updateAvailablePositions(Coordinate coord){
+        availablePositions.remove(coord);
+
+        Face facePlayed = grid.get(coord);
+
+        Content corner = null;
+        Coordinate check;
+        for(int i=-1; i<=1; i=i+2) {
+            for (int j = -1; j <= 1; j = j + 2) {
+                check = new Coordinate(coord.getX() + i, coord.getY() + j);
+                if(!grid.containsKey(check)){
+                    if(i==-1 && j==-1) //bottom left card
+                        corner = facePlayed.getBL();
+                    if(i==-1 && j==1) //top left card
+                        corner = facePlayed.getTL();
+                    if(i==1 && j==-1) //bottom right card
+                        corner = facePlayed.getBR();
+                    if(i==1 && j==1) //top right card
+                        corner = facePlayed.getTR();
+
+
+                    if(corner != Content.HIDDEN)
+                        availablePositions.add(check);
+                }else{
+                    if(i==-1 && j==-1) //bottom left card
+                        grid.get(check).setCoveredTR(true);
+                    if(i==-1 && j==1) //top left card
+                        grid.get(check).setCoveredBR(true);
+                    if(i==1 && j==-1) //bottom right card
+                        grid.get(check).setCoveredTL(true);
+                    if(i==1 && j==1) //top right card
+                        grid.get(check).setCoveredBL(true);
+                }
+            }
+        }
+    }
+
+    //RESORUCE GETTERS AND SETTERS
+    public int getPlantCount(){
+        return this.resourceCount[0];
+    }
+    public void setPlantCount(int count){
+        this.resourceCount[0] = count;
+    }
+    public int getAnimalCount(){
+        return this.resourceCount[1];
+    }
+    public void setAnimalCount(int count){
+        this.resourceCount[1] = count;
+    }
+    public int getFungiCount(){
+        return this.resourceCount[2];
+    }
+    public void setFungiCount(int count){
+        this.resourceCount[2] = count;
+    }
+    public int getInsectCount(){
+        return this.resourceCount[3];
+    }
+    public void setInsectCount(int count){
+        this.resourceCount[3] = count;
+    }
+
+    //OBJECT GETTERS AND SETTERS
+    public int getQuillCount(){
+        return this.objectCount[0];
+    }
+    public void setQuillCount(int count){
+        this.objectCount[0] = count;
+    }
+    public int getInkwellCount(){
+        return this.objectCount[1];
+    }
+    public void setInkwellCount(int count){
+        this.objectCount[1] = count;
+    }
+    public int getManuscriptCount(){
+        return this.objectCount[2];
+    }
+    public void setManuscriptCount(int count){
+        this.objectCount[2] = count;
+    }
+
 
     /*public int objPointsCalc(){
 
