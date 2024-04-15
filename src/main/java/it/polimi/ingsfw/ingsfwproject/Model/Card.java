3300d84918ab48d7a37c17943309e23ed5258b23
Beatrice Spazzadeschi <bea.spazza@gmail.com>
2024-04-15 17:58:28 +0200

    GetType in card

--- src/main/java/it/polimi/ingsfw/ingsfwproject/Model/Card.java
+++ src/main/java/it/polimi/ingsfw/ingsfwproject/Model/Card.java
@@ -10,4 +10,18 @@ abstract public class Card {
     public void setIdCard(int idCard) {
         this.idCard = idCard;
     }
+
+    public static Content getType(int id){
+        if(id<=10 || (id>=41 && id<=50))
+            return Content.FUNGI_KINGDOM;
+        else if(id <= 20 || (id >= 51 && id <= 60))
+            return Content.PLANT_KINGDOM;
+        else if(id<=30 || (id>=52 && id<=70))
+            return Content.ANIMAL_KINGDOM;
+        else if(id<=40 || (id>=71 && id<=80))
+            return Content.INSECT_KINGDOM;
+        else
+            return null;
+    }
+
 }
