99e437f757c9df0ff746a36318aea74e48a33568
Beatrice Spazzadeschi <bea.spazza@gmail.com>
2024-04-21 18:00:38 +0200

    Player - play card

---
+++ src/main/java/it/polimi/ingsfw/ingsfwproject/Exceptions/CardNotInHandException.java
@@ -0,0 +1,8 @@
+package it.polimi.ingsfw.ingsfwproject.Exceptions;
+
+public class CardNotInHandException extends Throwable {
+    public CardNotInHandException(String message) {
+        super(message);
+    }
+    }
+}
