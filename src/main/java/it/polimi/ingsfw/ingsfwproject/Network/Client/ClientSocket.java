71ffdcd0829f982b55b990e2cd3e98677c514a25
Beatrice Spazzadeschi <bea.spazza@gmail.com>
2024-04-22 17:15:11 +0200

    Client Socket

--- src/main/java/it/polimi/ingsfw/ingsfwproject/Network/Client/ClientSocket.java
+++ src/main/java/it/polimi/ingsfw/ingsfwproject/Network/Client/ClientSocket.java
@@ -1,41 +1,67 @@
 package it.polimi.ingsfw.ingsfwproject.Network.Client;
 
+import it.polimi.ingsfw.ingsfwproject.Network.Actions.Message;
+
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.InetSocketAddress;
 import java.net.Socket;
-import java.util.Timer;
+import java.rmi.RemoteException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
-public class ClientSocket {
-    private String ip;
-    private int port;
+public class ClientSocket extends Client implements Runnable{
     private ObjectInputStream input;
     private ObjectOutputStream output;
     private Socket socket;
-    //Dove mettere timer per timeout??
-//    private Timer timer;
-//    private static final int timerduration=4000;
 
-    public ClientSocket(String ip, int port) throws IOException {
-        this.ip = ip;
-        this.port = port;
-        this.socket=new Socket(ip,port);
-        input=new ObjectInputStream(socket.getInputStream());
-        output = new ObjectOutputStream(socket.getOutputStream());
-        this.socket.connect(new InetSocketAddress(ip,port));
+    public ClientSocket(String nickname, String ip, int port){
+        super(ip,port,nickname);
+    }
+
+    @Override
+    public void startConnection() throws IOException {
+        this.socket = new Socket();
+        this.socket.connect(new InetSocketAddress(getIp(), getPort()));
+        this.output = new ObjectOutputStream(socket.getOutputStream());
+        this.input = new ObjectInputStream(socket.getInputStream());
 
     }
 
-    public void read(){
-
+    //Send message to server
+    @Override
+    public void sendMessage(Message message) throws IOException {
+        try {
+            output.writeObject(message);
+            output.flush();
+            output.reset();
+        } catch (IOException e) {
+            e.printStackTrace();
+            disconnect();
+        }
     }
 
-    public void send(){
-
+    @Override
+    public void disconnect(){
+        getReadExecutionQueue().shutdown();
+        try {
+            if (!socket.isClosed()) {
+                socket.close();
+            }
+        } catch (IOException e) {
+            System.out.println("Disconnection failed");
+            e.printStackTrace();
+        }
     }
 
-    public void close() throws IOException {
-        socket.close();
+    @Override
+    protected Message receiveMessage() throws IOException, ClassNotFoundException {
+        return (Message) input.readObject();
+    }
+
+    @Override
+    public void run() {
+
     }
 }
