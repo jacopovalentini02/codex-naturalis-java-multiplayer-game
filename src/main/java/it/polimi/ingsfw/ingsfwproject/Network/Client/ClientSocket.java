93a37ad4dae27cbbdc6398025bd21567df982101
Beatrice Spazzadeschi <bea.spazza@gmail.com>
2024-04-23 10:23:30 +0200

    Client Socket

--- src/main/java/it/polimi/ingsfw/ingsfwproject/Network/Client/ClientSocket.java
+++ src/main/java/it/polimi/ingsfw/ingsfwproject/Network/Client/ClientSocket.java
@@ -7,7 +7,6 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.InetSocketAddress;
 import java.net.Socket;
-import java.rmi.RemoteException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
