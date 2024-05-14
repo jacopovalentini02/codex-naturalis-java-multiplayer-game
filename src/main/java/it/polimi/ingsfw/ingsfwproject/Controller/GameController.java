3e74f37088817b08e9827457dbc62ae71c8dcb20
Beatrice Spazzadeschi <bea.spazza@gmail.com>
2024-05-14 14:03:07 +0200

    boolean funzioni

--- src/main/java/it/polimi/ingsfw/ingsfwproject/Controller/GameController.java
+++ src/main/java/it/polimi/ingsfw/ingsfwproject/Controller/GameController.java
@@ -137,7 +137,7 @@ public class GameController implements Controller {
             deck = model.getGoldDeck();
         }
 
-        checkIfDrawPossible(player);
+        checkIfDrawPossible(player);//
 
         if (deck.equals(model.getObjectiveDeck())){
             serverInstance.sendUpdateToAll(new ExcpetionMessage(player.getClientID(),"You can't draw from objective deck!"));
