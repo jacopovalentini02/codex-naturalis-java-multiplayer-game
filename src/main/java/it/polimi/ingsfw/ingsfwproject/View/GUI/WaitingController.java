6e3d0743496c535cb27b93e44566fc5b3eef87f8
jacopovalentini02 <jacopogiuseppe.valentini@mail.polimi.it>
2024-05-22 22:21:36 +0200

    Aggiunta chat nella waiting. Funziona ma va refactorata

--- src/main/java/it/polimi/ingsfw/ingsfwproject/View/GUI/WaitingController.java
+++ src/main/java/it/polimi/ingsfw/ingsfwproject/View/GUI/WaitingController.java
@@ -1,13 +1,25 @@
 package it.polimi.ingsfw.ingsfwproject.View.GUI;
 
+import it.polimi.ingsfw.ingsfwproject.Model.ChatMessage;
+import it.polimi.ingsfw.ingsfwproject.Network.Messages.ClientToServer.sendChatMessage;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
+import javafx.scene.control.*;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+import static it.polimi.ingsfw.ingsfwproject.View.View.client;
 
 public class WaitingController extends GUIController{
 
@@ -16,9 +28,27 @@ public class WaitingController extends GUIController{
     @FXML
     public Label playerNickname;
     @FXML
+    public ComboBox<String> chatSelector;
+    @FXML
+    public TextField chatTextField;
+    @FXML
+    public Button sendButton;
+    @FXML
+    private ListView<ChatMessage> currentChat;
+    @FXML
     private TextArea newPlayerJoined;
 
+    @FXML
+    private Button showChat;
+
+    @FXML
+    private AnchorPane chatPane;
+
     public static GUIView guiView;
+
+    private ObservableList<String> chatOptions;
+
+    private Map<String, ObservableList<ChatMessage>> chats;
 
     public void start(Stage stage) throws Exception {
         setGuiView(guiView);
@@ -32,6 +62,31 @@ public class WaitingController extends GUIController{
         stage.setScene(scene);
         //stage.setMaximized(true);
         stage.show();
+    }
+    @FXML
+    private void initialize(){
+        chatOptions = FXCollections.observableArrayList();
+        chatSelector.setItems(chatOptions);
+        chatOptions.add("global");
+        chats = new HashMap<>();
+        chats.put("global", FXCollections.observableArrayList());
+        chatSelector.getSelectionModel().select("global");
+        chatSelector.setOnAction(this::changeChat);
+
+
+        currentChat.setCellFactory(param -> new ListCell<ChatMessage>() {
+            @Override
+            protected void updateItem(ChatMessage item, boolean empty) {
+                super.updateItem(item, empty);
+                if (empty || item == null) {
+                    setText(null);
+                } else {
+                    setText(item.toString());
+                }
+            }
+        });
+
+        updateCurrentChat();
     }
 
     public static void setGuiView(GUIView view) {
@@ -49,6 +104,70 @@ public class WaitingController extends GUIController{
     public void setPlayerNickname(String playerNickname) {
         this.playerNickname.setText("Welcome "+playerNickname+"!");
     }
+
+    @FXML
+    private void changeChat(ActionEvent event){
+        updateCurrentChat();
+    }
+
+    private void updateCurrentChat() {
+        String selectedChat = chatSelector.getSelectionModel().getSelectedItem();
+        if (selectedChat != null && chats.containsKey(selectedChat)) {
+            currentChat.setItems(chats.get(selectedChat));
+        }
+    }
+
+    public void addMessageToChat(ChatMessage message){
+        String sender = message.getSender();
+        String key;
+
+        if (sender.equals(client.getNickname()) || message.getRecipient().equals("global")){
+             key = message.getRecipient();
+        } else {
+             key = message.getSender();
+        }
+
+        if (chats.containsKey(key)){
+            chats.get(key).add(message);
+            if (chatSelector.getSelectionModel().getSelectedItem().equals(key)){
+                updateCurrentChat();
+            }
+        }
+    }
+
+    @FXML
+    public void sendMessage(ActionEvent event) throws IOException {
+        String messageText = chatTextField.getText();
+        if (messageText != null && !messageText.trim().isEmpty()){
+            String selectedChat = chatSelector.getSelectionModel().getSelectedItem();
+            String author = client.getNickname();
+            ChatMessage message = new ChatMessage(author, selectedChat, messageText);
+            if (message.getRecipient() != "global"){
+                addMessageToChat(message);
+            }
+            client.sendMessage(new sendChatMessage(client.getClientID(),author, selectedChat, messageText));
+            chatTextField.clear();
+        }
+    }
+
+    @FXML
+    public void toggleChatMenu(ActionEvent actionEvent) {
+        if (chatPane.isVisible()){
+            chatPane.setVisible(false);
+            showChat.setText("Show Chat");
+        } else {
+            chatPane.setVisible(true);
+            showChat.setText("Hide chat");
+        }
+    }
+
+    public void addChat(String nickname){
+        if (!Objects.equals(nickname, client.getNickname()) && !(chats.containsKey(nickname))){
+            chatOptions.add(nickname);
+            chats.put(nickname, FXCollections.observableArrayList());
+        }
+    }
+
 }
 
 
